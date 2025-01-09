ae39f69164ba4c0dd16a90e07e348aaaf9912e3d
Khalil-keita <keitakalil200@icloud.com>
2025-01-09 12:28:23 +0000

    le kernel

--- src/kernel.php
+++ src/kernel.php
@@ -3,13 +3,14 @@ namespace Khalil;
 
 use Khalil\Components\Http\Interface\RequestInterface;
 use Khalil\Components\Http\Interface\ResponseInterface;
+use Khalil\Components\Http\Response;
 
 
 final class Kernel{
 
     public function handle(RequestInterface $request): ResponseInterface
     {
-
+        return new Response(); //TODO
     }
 
 }
