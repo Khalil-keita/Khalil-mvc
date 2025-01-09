68b5cd53cbb3331d1bed0369b0dd9f3bf45fabe1
Khalil-keita <132109395+Khalil-keita@users.noreply.github.com>
2025-01-09 11:53:45 +0000

    Initialisation des composants http

---
+++ src/kernel.php
@@ -0,0 +1,13 @@
+<?php
+namespace Khalil;
+
+use Khalil\Components\Http\Interface {RequestInterface, ResponseInterface};
+
+final class Kernel{
+
+    public function handle(RequestInterface $request): ResponseInterface
+    {
+
+    }
+
+}
